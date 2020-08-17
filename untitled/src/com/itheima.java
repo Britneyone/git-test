00be023f114038760e1676eddf6a9407e45eaeff
ShiBei <ShiBei@qq.com>
2020-08-17 22:55:23 +0800

    把本地工程添加到git

---
+++ untitled/src/com/itheima.java
@@ -0,0 +1,12 @@
+package com;
+
+public class itheima {
+    public static void main(String[] args) {
+        System.out.println("hello 1");
+        System.out.println("hello 1");
+        System.out.println("hello 1");
+        System.out.println("hello 1");
+        System.out.println("hello 1");
+        System.out.println("hello 1");
+    }
+}
